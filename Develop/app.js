cae8425065fe227c7170318681f1e3a22b75a8bd
Chris Schramm <schrammcj@gmail.com>
2020-09-07 14:48:16 -0400

    Pushing updates with full functionality

--- Develop/app.js
+++ Develop/app.js
@@ -4,57 +4,26 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
+let employees = [];
 // array of questions for user
-const questions = [
-
+const roleQs = [
   {
     type: "list",
     name: "role",
     message: "Which type of employee?",
-    choices: ["Manager", "Engineer", "Intern"]
+    choices: ["Engineer", "Intern", "Team Completed"],
   },
-  // {
-  //   type: "input",
-  //   name: "managerId",
-  //   message: "Manager's ID?",
-  // },
-  // {
-  //   type: "input",
-  //   name: "officeNum",
-  //   message: "Office number of Team-Lead/manager?",
-  // },
-  // {
-  //   type: "number",
-  //   name: "internNum",
-  //   message: "How many interns on development team?",
-  // },
-  // {
-  //   type: "input",
-  //   name: "school",
-  //   message: "What is the school for intern + ' '?",
-  // },
-  // {
-  //   type: "number",
-  //   name: "engineerNum",
-  //   message: "How many engineers on development team?",
-  // },
-  // {
-  //   type: "input",
-  //   name: "github",
-  //   message: "What is the GitHub username for engineer + ' '?",
-  // },
-]
+];
 const managerQs = [
   {
     type: "input",
     name: "name",
-    message: "Name of employee?",
+    message: "Name of manager?",
   },
   {
     type: "input",
     name: "id",
-    message: "ID of employee?",
+    message: "ID of manager?",
   },
   {
     type: "input",
@@ -66,40 +35,39 @@ const managerQs = [
     name: "officeNumber",
     message: "Office number of manager?",
   },
-]
-
-const engineerQs = [{
-  type: "input",
-  name: "name",
-  message: "Name of employee?",
-},
-{
-  type: "input",
-  name: "id",
-  message: "ID of employee?",
-},
-{
-  type: "input",
-  name: "emailE",
-  message: "What is the email for engineer?",
-},
-{
-  type: "input",
-  name: "github",
-  message: "What is the GitHub username for engineer?",
-},
-]
-
-const internQs = [
+];
+const engineerQs = [
   {
     type: "input",
     name: "name",
-    message: "Name of employee?",
+    message: "Name of engineer?",
   },
   {
     type: "input",
     name: "id",
-    message: "ID of employee?",
+    message: "ID of engineer?",
+  },
+  {
+    type: "input",
+    name: "emailE",
+    message: "What is the email for engineer?",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is the GitHub username for engineer?",
+  },
+];
+const internQs = [
+  {
+    type: "input",
+    name: "name",
+    message: "Name of intern?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "ID of intern?",
   },
   {
     type: "input",
@@ -110,76 +78,80 @@ const internQs = [
     type: "input",
     name: "school",
     message: "What is the school for intern?",
-  },]
-
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-
+  },
+];
+const OUTPUT_DIR = path.resolve(__dirname, "output"); //locates folder dir
+const outputPath = path.join(OUTPUT_DIR, "team.html"); //creates file
 const render = require("./lib/htmlRenderer");
+function createManager(response) {
+  const aManager = new Manager(
+    response.name,
+    response.id,
+    response.emailM,
+    response.officeNumber
+  );
+  employees.push(aManager);
+  console.log(aManager.getName());
+}
 function engineerAsk() {
   inquirer.prompt(engineerQs).then(function (response) {
-    const anEngineer = new Engineer(response.name, response.id, response.emailE, response.github)
-    console.log(anEngineer.getName())
-  })
-}
-function managerAsk() {
-  inquirer.prompt(managerQs).then(function (response) {
-    const aManager = new Manager(response.name, response.id, response.emailM, response.officeNumber)
-    console.log(aManager.getName())
-  })
+    const anEngineer = new Engineer(
+      response.name,
+      response.id,
+      response.emailE,
+      response.github
+    );
+    console.log(anEngineer.getName());
+    employees.push(anEngineer);
+    teamQs();
+  });
 }
 function internAsk() {
   inquirer.prompt(internQs).then(function (response) {
-    const anIntern = new Intern(response.name, response.id, response.emailI, response.school)
-    console.log(anIntern.getName())
-  })
+    const anIntern = new Intern(
+      response.name,
+      response.id,
+      response.emailI,
+      response.school
+    );
+    console.log(anIntern.getName());
+    employees.push(anIntern);
+    teamQs();
+  });
 }
-function init() {
-  inquirer.prompt(questions).then(function (response) {
+function teamQs() {
+  inquirer.prompt(roleQs).then(function (response) {
     switch (response.role) {
-      case "Manager":
-        // code block
-        managerAsk();
-        break;
       case "Engineer":
-        // code block
         engineerAsk();
         break;
-      default:
-        // code block
+      case "Intern":
         internAsk();
+        break;
+      default:
+        makeTeam();
     }
-    function createManager() { }
-    function createTeam() { }
-    function createEngineer() { }
-    function createIntern() { }
-    function renderHTML() { }
-    createManager();
-  })
+  });
 }
-
+function makeTeam() {
+  if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR);
+  }
+  fs.writeFileSync(outputPath, render(employees), "utf-8");
+  process.exit(0);
+}
+function init() {
+  inquirer.prompt(managerQs).then(function (response) {
+    createManager(response);
+    teamQs();
+  });
+}
+// function init() {
+//   inquirer.prompt(managerQs).then(function (response) {
+//     createManager(response)
+//     "choices" =
+//     while (roleQs != "Team Completed") { teamQs() }
+//   })
+// }
 // function call to initialize program
 init();
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
